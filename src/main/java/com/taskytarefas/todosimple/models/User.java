4f1bb4631d689cdfbf29c2c39743bb6dcf6c86fc
Christyan <christyan.assolini@gmail.com>
2026-02-03 15:07:04 -0300

    Update

---
+++ src/main/java/com/taskytarefas/todosimple/models/User.java
@@ -0,0 +1,118 @@
+package com.taskytarefas.todosimple.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.apache.logging.log4j.spi.ObjectThreadContextMap;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "user")
+public class User {
+    public interface CreateUser {
+    }
+    public interface UpdateUser {
+    }
+
+    public static final String TABLE_NAME = "user";
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "nome", length = 100, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    private String nome;
+
+    @Column(name = "email", length = 255, nullable = false, unique = true)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    private String email;
+
+    @Column(name = "senha", length = 60, nullable = false)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 6, max = 60)
+    private String senha;
+
+    public User() {
+    }
+
+    public User(Long id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+    if (this == obj)
+        return true;
+
+    if (obj == null)
+        return false;
+
+    if (!(obj instanceof User))
+        return false;
+
+    User other = (User) obj;
+
+    if (this.id != null && other.id != null) {
+        return Objects.equals(this.id, other.id);
+    }
+
+    return Objects.equals(this.email, other.email);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
+        return result;
+    }
+}
+
